6f299b7b2cd9fd9e710d11cfa5d775abe0c57e21
Jeevan Kumar Pappala <pjeevankumar225@gmail.com>
2025-06-24 23:53:28 +0530

    redirect to app on gh pages

--- WebApp/vite.config.js
+++ WebApp/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import fs from 'fs';
 
+function copyIndexTo404() {
+  return {
+    name: 'copy-index-to-404',
+    closeBundle() {
+      const indexPath = resolve(__dirname, 'dist/index.html');
+      const notFoundPath = resolve(__dirname, 'dist/404.html');
+      fs.copyFileSync(indexPath, notFoundPath);
+    },
+  };
+}
 export default defineConfig({
   base: '/kpj-tshirts',
-  plugins: [react()],
+  plugins: [react(), copyIndexTo404()],
 });
